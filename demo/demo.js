c890e3e8268721ff5779356007c7cdb2d474845c
n <e>
2016-04-04 22:11:20 +0000

    Wait for sources to be loaded before computing recur events

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,19 @@
 ics_sources = ['samples/events.ics','samples/32c3.ics','samples/daily_recur.ics']
 
+
 function data_req (url, callback) {
     req = new XMLHttpRequest()
     req.addEventListener('load', callback)
     req.open('GET', url)
     req.send()
+}
+
+function add_recur_events() {
+    if (sources_to_load_cnt < 1) {
+        $('#calendar').fullCalendar('addEventSource', expand_recur_events)
+    } else {
+        setTimeout(add_recur_events, 30)
+    }
 }
 
 $(document).ready(function() {
@@ -16,11 +25,13 @@ $(document).ready(function() {
         },
         defaultView: 'month',
     })
+    sources_to_load_cnt = ics_sources.length
     for (ics of ics_sources) {
         data_req(ics, function(){
             $('#calendar').fullCalendar('addEventSource', fc_events(this.response))
+            sources_to_load_cnt -= 1
         })
     }
-    $('#calendar').fullCalendar('addEventSource', expand_recur_events)
+    add_recur_events()
 })
 
